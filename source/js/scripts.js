7a6f1ca0531353466c83dd1b70da8d0670b40baa
StasyaV <46928311+StasyaV@users.noreply.github.com>
2020-04-11 21:09:24 +0300

    Обновлены файлы

---
+++ source/js/scripts.js
@@ -0,0 +1,18 @@
+var mainMenu = document.querySelector('.main-nav');
+var menuToggle = document.querySelector('.menu-button');
+
+mainMenu.classList.remove('main-nav--no-js');
+menuToggle.classList.remove('menu-button--none');
+
+menuToggle.addEventListener('click', function () {
+  if (mainMenu.classList.contains('main-nav--closed')) {
+    mainMenu.classList.remove('main-nav--closed');
+    mainMenu.classList.add('main-nav--open');
+    menuToggle.classList.add('menu-button--open');
+  } else {
+    mainMenu.classList.add('main-nav--closed');
+    mainMenu.classList.remove('main-nav--open');
+    menuToggle.classList.remove('menu-button--open');
+    menuToggle.classList.add('menu-button--closed');
+  }
+});
