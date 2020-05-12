5227927d76592ef032a40fcac3900e4bc1ae8041
StasyaV <46928311+StasyaV@users.noreply.github.com>
2020-05-12 18:52:26 +0300

    Добавлен скрипт проверки формы

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,5 +1,14 @@
 var mainMenu = document.querySelector('.main-nav');
 var menuToggle = document.querySelector('.menu-button');
+var form = document.querySelector(".form");
+var name = form.querySelector("[name=name]");
+var nameField = form.querySelector(".cat-info__name");
+var weight = form.querySelector("[name=weight]");
+var weightField = form.querySelector(".cat-info__weight");
+var email = form.querySelector("[name=email]");
+var contacts = form.querySelector(".form__contacts-data");
+var phone = form.querySelector("[name=phone]");
+var error = form.querySelector(".error");
 
 mainMenu.classList.remove('main-nav--no-js');
 menuToggle.classList.remove('menu-button--none');
@@ -14,5 +23,21 @@ menuToggle.addEventListener('click', function () {
     mainMenu.classList.remove('main-nav--open');
     menuToggle.classList.remove('menu-button--open');
     menuToggle.classList.add('menu-button--closed');
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!name.value) {
+    evt.preventDefault();
+    nameField.classList.add("error");
+    console.log("Нужно ввести имя кота");
+  } if (!weight.value) {
+    evt.preventDefault();
+    weightField.classList.add("error");
+    console.log("Нужно ввести вес кота");
+  } if (!email.value || !phone.value) {
+    evt.preventDefault();
+    contacts.classList.add("error");
+    console.log("Нужно ввести контактные данные");
   }
 });
