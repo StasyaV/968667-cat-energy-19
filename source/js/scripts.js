c801cd64649a60563ecc9d6c74eeb3ef96473522
StasyaV <46928311+StasyaV@users.noreply.github.com>
2020-05-14 10:35:00 +0300

    Правки в js

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,48 +1,38 @@
-var mainMenu = document.querySelector('.main-nav');
-var menuToggle = document.querySelector('.menu-button');
-var form = document.querySelector(".form");
-var nameInputs = form.querySelectorAll(".cat-info__input");
-var contactsInputs = form.querySelectorAll(".form__contacts-data-input");
+window.addEventListener("load", function () {
+  var mainMenu = document.querySelector('.main-nav');
+  var menuToggle = document.querySelector('.menu-button');
+  var form = document.querySelector(".form");
+  var infoInputs = form.querySelectorAll(".info-input");
 
 
-mainMenu.classList.remove('main-nav--no-js');
-menuToggle.classList.remove('menu-button--none');
+  mainMenu.classList.remove('main-nav--no-js');
+  menuToggle.classList.remove('menu-button--none');
 
-menuToggle.addEventListener('click', function () {
-  if (mainMenu.classList.contains('main-nav--closed')) {
-    mainMenu.classList.remove('main-nav--closed');
-    mainMenu.classList.add('main-nav--open');
-    menuToggle.classList.add('menu-button--open');
-  } else {
-    mainMenu.classList.add('main-nav--closed');
-    mainMenu.classList.remove('main-nav--open');
-    menuToggle.classList.remove('menu-button--open');
-    menuToggle.classList.add('menu-button--closed');
-  }
-});
-
-nameInputs.forEach(function (input) {
-  input.addEventListener("blur", function (evt) {
-    if (!input.value) {
-      input.parentNode.classList.add("error");
-      console.log("Нужно ввести имя кота");
+  menuToggle.addEventListener('click', function () {
+    if (mainMenu.classList.contains('main-nav--closed')) {
+      mainMenu.classList.remove('main-nav--closed');
+      mainMenu.classList.add('main-nav--open');
+      menuToggle.classList.add('menu-button--open');
     } else {
-      input.parentNode.classList.remove("error");
+      mainMenu.classList.add('main-nav--closed');
+      mainMenu.classList.remove('main-nav--open');
+      menuToggle.classList.remove('menu-button--open');
+      menuToggle.classList.add('menu-button--closed');
     }
   });
-});
 
-contactsInputs.forEach(function (input) {
-  input.addEventListener("blur", function (evt) {
-    if (!input.value) {
-      input.parentNode.classList.add("error");
-      console.log("Нужно ввести контактные данные");
-    } else {
-      input.parentNode.classList.remove("error");
-    }
+  infoInputs.forEach(function (input) {
+    input.addEventListener("blur", function (evt) {
+      if (!input.value) {
+        input.parentNode.classList.add("error");
+        console.log("Нужно ввести имя кота");
+      } else {
+        input.parentNode.classList.remove("error");
+      }
+    });
   });
-});
 
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
+  form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+  });
 });
